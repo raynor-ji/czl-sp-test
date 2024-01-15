2939649e6565d738dad545ef5d540238f29ded59
DaeHan Ji <han@DaeHans-MacBook-Pro.local>
2024-01-15 13:18:11 +0900

    init

---
+++ src/main/java/com/czl/ClzSpTestApplication.java
@@ -0,0 +1,13 @@
+package com.czl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClzSpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClzSpTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czl/ClzSpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClzSpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
